40da11fa705bb009654c7f08878cc864ab4dfd6e
robwannell <rob@meltemiweb.com>
2016-01-14 11:12:14 -0800

    rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,8 @@ set :repo_url, 'git@github.com:robwannell/mattwilliams.git'
 
 set :deploy_to, '/home/deploy/matt'
 
+set :rbenv_path, '/home/deploy/.rbenv/'
+
 set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
